3ab3b63564e60e47aeecf99c01a7392429135318
BrianFHS <bri.hurtado@duocuc.cl>
2026-07-07 20:17:02 -0400

    Creación menu

---
+++ app.py
@@ -0,0 +1,10 @@
+def mostrar_menu():
+    print("========== MENÚ PRINCIPAL ==========")
+    print("1. Stock por categoría")
+    print("2. Buscar productos por rango de precio")
+    print("3. Actualizar precio")
+    print("4. Agregar producto")
+    print("5. Eliminar producto")
+    print("6. Mostrar productos")
+    print("7. Salir")
+    print("===================================")
